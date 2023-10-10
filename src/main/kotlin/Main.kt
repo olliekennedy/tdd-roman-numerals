7474e91512681d9794fa57584d779fdabb0c464c
Ollie Kennedy <ollie.kennedy1@gmail.com>
2023-10-10 16:25:56 +0100

    feat: calculate roman numerals up to 11

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,13 @@ fun main(number: Int): String? {
         2 to "II",
         3 to "III",
         4 to "IV",
+        5 to "V",
+        6 to "VI",
+        7 to "VII",
+        8 to "VIII",
+        9 to "IX",
+        10 to "X",
+        11 to "XI",
     )
     return numberToNumeralMapping[number]
 }
