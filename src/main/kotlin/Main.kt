4481e2e7e0b7b9148e0fc2bf9f5d9e5d999cfdca
Ollie Kennedy <ollie.kennedy1@gmail.com>
2023-10-10 16:41:18 +0100

    refactor: extract and rename mapping so it reads like english when called

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,22 @@
 fun main(number: Int): String? {
-    val numberToNumeralMapping = mapOf(
-        0 to "",
-        1 to "I",
-        2 to "II",
-        3 to "III",
-        4 to "IV",
-        5 to "V",
-        6 to "VI",
-        7 to "VII",
-        8 to "VIII",
-        9 to "IX",
-    )
     var output = ""
-    if (number >= 10) {
-        output += "X"
-    }
-    output += numberToNumeralMapping[number % 10]
+
+    if (number >= 10) output += "X"
+
+    output += numeralFor[number % 10]
+
     return output
 }
+
+val numeralFor = mapOf(
+    0 to "",
+    1 to "I",
+    2 to "II",
+    3 to "III",
+    4 to "IV",
+    5 to "V",
+    6 to "VI",
+    7 to "VII",
+    8 to "VIII",
+    9 to "IX",
+)
