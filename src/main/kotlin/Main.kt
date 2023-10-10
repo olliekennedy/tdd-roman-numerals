5e475cbbb05a19b503b247a112a4cdda1a161d27
Ollie Kennedy <ollie.kennedy1@gmail.com>
2023-10-10 16:58:33 +0100

    feat: numerals up to 99

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -26,4 +26,11 @@ val tensNumeralFor = mapOf(
     0 to "",
     1 to "X",
     2 to "XX",
+    3 to "XXX",
+    4 to "XL",
+    5 to "L",
+    6 to "LX",
+    7 to "LXX",
+    8 to "LXXX",
+    9 to "XC",
 )
