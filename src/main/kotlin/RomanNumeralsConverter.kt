c37a3838579cd03611d7587565a0f96ca55bbfb6
Ollie Kennedy <ollie.kennedy1@gmail.com>
2023-10-11 13:26:51 +0100

    feat: up to 99

--- src/main/kotlin/RomanNumeralsConverter.kt
+++ src/main/kotlin/RomanNumeralsConverter.kt
@@ -1,6 +1,6 @@
 private const val MIN_NUMBER = 0
 private const val MAX_NUMBER = 3999
-private val VALID_NUMERALS = listOf('I', 'V', 'X')
+private val VALID_NUMERALS = listOf('I', 'V', 'X', 'L', 'C')
 
 fun romanNumeralsFromNumber(number: Int): String? {
     if (number.isOutOfRange()) return null
@@ -11,9 +11,28 @@ fun romanNumeralsFromNumber(number: Int): String? {
 }
 
 fun numberFromRomanNumerals(numeral: String): Int? {
+    var numeralRemainder = numeral
+    var onesNumerals = ""
+    var tensNumerals = ""
     if (numeral.any { it !in VALID_NUMERALS }) return null
 
-    return numeral.toNumberConsidering(1)
+    for ((index, char) in numeralRemainder.withIndex()) {
+        if (char in listOf(digitToNumeralsMapping[1]!![1], digitToNumeralsMapping[1]!![5])) {
+            onesNumerals = numeralRemainder.substring(index, numeralRemainder.length)
+            numeralRemainder = numeralRemainder.substring(0, index)
+            break
+        }
+    }
+
+    for ((index, char) in numeralRemainder.withIndex()) {
+        if (char in listOf(digitToNumeralsMapping[10]!![1], digitToNumeralsMapping[10]!![5])) {
+            tensNumerals = numeralRemainder.substring(index, numeralRemainder.length)
+            numeralRemainder = numeralRemainder.substring(0, index)
+            break
+        }
+    }
+
+    return onesNumerals.toNumberConsidering(1) + tensNumerals.toNumberConsidering(10)
 }
 
 private fun Int.isOutOfRange() = this < MIN_NUMBER || this > MAX_NUMBER
@@ -26,19 +45,19 @@ private fun Int.toNumeralsConsidering(digit: Int): String? {
     return numeralsMappingCalculatedUsing(charMapping)[this]
 }
 
-private fun String.toNumberConsidering(digit: Int): Int? {
+private fun String.toNumberConsidering(digit: Int): Int {
     val charMapping = digitToNumeralsMapping[digit]!!
 
     val numberMapping = numberMappingCalculatedUsing(charMapping)
 
-    return numberMapping[this]
+    return digit * (numberMapping[this] ?: 0)
 }
 
-private fun numberMappingCalculatedUsing(charMapping: Map<Int, String>): Map<String, Int> {
+private fun numberMappingCalculatedUsing(charMapping: Map<Int, Char>): Map<String, Int> {
     return numeralsMappingCalculatedUsing(charMapping).map { (k, v) -> v to k }.toMap()
 }
 
-private fun numeralsMappingCalculatedUsing(charMapping: Map<Int, String>) = mapOf(
+private fun numeralsMappingCalculatedUsing(charMapping: Map<Int, Char>) = mapOf(
     0 to "",
     1 to "${charMapping[1]}",
     2 to "${charMapping[1]}${charMapping[1]}",
@@ -52,8 +71,8 @@ private fun numeralsMappingCalculatedUsing(charMapping: Map<Int, String>) = mapO
 )
 
 private val digitToNumeralsMapping = mapOf(
-    1 to mapOf(1 to "I", 5 to "V", 10 to "X"),
-    10 to mapOf(1 to "X", 5 to "L", 10 to "C"),
-    100 to mapOf(1 to "C", 5 to "D", 10 to "M"),
-    1000 to mapOf(1 to "M"),
+    1 to mapOf(1 to 'I', 5 to 'V', 10 to 'X'),
+    10 to mapOf(1 to 'X', 5 to 'L', 10 to 'C'),
+    100 to mapOf(1 to 'C', 5 to 'D', 10 to 'M'),
+    1000 to mapOf(1 to 'M'),
 )
